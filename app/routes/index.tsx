e1d46f5ec83ca52f3cc356803a616c89dbae7157
m7he4rt <mateuspm2015@hotmail.com>
2022-12-16 23:14:02 -0300

    feat: basic architeture with remix

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div className="bg-purple-900 min-h-screen min-w-full flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-white">
+        He4rt Learning SSR with Remix...
+      </h1>
+    </div>
+  );
+}
