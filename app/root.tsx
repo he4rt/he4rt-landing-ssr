bee0f7569cb724b86fbe96b66bb4713f17897e8c
m7he4rt <mateuspm2015@hotmail.com>
2022-12-17 00:58:59 -0300

    feat: create tailwind theme

--- app/root.tsx
+++ app/root.tsx
@@ -10,22 +10,42 @@ import {
 
 import styles from "~/tailwind.css";
 
+import favicon from "../public/favicon.svg";
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "He4rt Learning",
   viewport: "width=device-width,initial-scale=1",
 });
 
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700&family=Spline+Sans:wght@400;500;700&display=swap",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "icon",
+    href: favicon,
+    type: "image/svg",
+  },
+];
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <head>
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="w-screen h-screen dark:bg-gray-999 font-spline">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
