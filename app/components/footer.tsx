89eeb75f57041a6a811ada8b6a18a082d9d4b6a6
m7he4rt <mateuspm2015@hotmail.com>
2022-12-17 00:59:41 -0300

    feat: create a footer component

---
+++ app/components/footer.tsx
@@ -0,0 +1,128 @@
+import clsx from "clsx";
+import {
+  AiOutlineInstagram,
+  AiOutlineTwitter,
+  AiOutlineGithub,
+} from "react-icons/ai";
+import { FaDiscord } from "react-icons/fa";
+import type { ComponentProps } from "react";
+
+type LinkProps = Pick<
+  ComponentProps<"a">,
+  "children" | "href" | "target" | "aria-label"
+> & {
+  isIcon?: boolean;
+};
+
+const anchorLinks: Omit<LinkProps, "isIcon">[] = [
+  {
+    href: "#inicio",
+    children: "Início",
+  },
+  {
+    href: "#4noobs",
+    children: "4Noobs",
+  },
+  {
+    href: "#sobre",
+    children: "Sobre",
+  },
+];
+
+const socialMediaLinks: LinkProps[] = [
+  {
+    href: "https://www.instagram.com/heartdevs/",
+    children: (
+      <AiOutlineInstagram
+        size={24}
+        className="hover:-translate-y-0.5 transform-gpu transition-all hover:fill-purple-500 footer__icons"
+      />
+    ),
+    target: "_blank",
+    isIcon: true,
+    "aria-label": "Instagram da He4rtDevs",
+  },
+  {
+    href: "https://twitter.com/He4rtDevs",
+    children: (
+      <AiOutlineTwitter
+        size={24}
+        className="hover:-translate-y-0.5 transform-gpu transition-all hover:fill-purple-500 footer__icons"
+      />
+    ),
+    target: "_blank",
+    isIcon: true,
+    "aria-label": "Twitter da He4rtDevs",
+  },
+  {
+    href: "https://discord.com/invite/5kwDQuv",
+    children: (
+      <FaDiscord
+        size={24}
+        className="hover:-translate-y-0.5 transform-gpu transition-all hover:fill-purple-500 footer__icons"
+      />
+    ),
+    target: "_blank",
+    isIcon: true,
+    "aria-label": "Discord da He4rtDevs",
+  },
+  {
+    href: "https://github.com/he4rt",
+    children: (
+      <AiOutlineGithub
+        size={24}
+        className="hover:-translate-y-0.5 transform-gpu transition-all hover:fill-purple-500 footer__icons"
+      />
+    ),
+    target: "_blank",
+    isIcon: true,
+    "aria-label": "Github da He4rtDevs",
+  },
+];
+
+const FooterLink = ({
+  children,
+  isIcon,
+  target = "_self",
+  ...props
+}: LinkProps) => {
+  const iconClass = "scroll-bottom-2 mx-5";
+  const textClass = "font-semibold hover:text-purple-500 md:text-base";
+
+  return (
+    <a
+      target={target}
+      rel="noopener noreferrer"
+      className={clsx(!isIcon && textClass, iconClass)}
+      {...props}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default function Footer() {
+  return (
+    <footer className="w-full bg-blue-900 dark:bg-gray-100 flex items-center justify-center">
+      <div className="flex flex-col text-gray-300 my-10 md:my-16">
+        <ul className="flex scroll-bottom-1 justify-between font-sans text-sm mx-5 md:mx-0">
+          {anchorLinks.map(({ children, href }) => (
+            <li key={href}>
+              <FooterLink href={href}>{children}</FooterLink>
+            </li>
+          ))}
+        </ul>
+        <div className="flex justify-center items-center my-10">
+          {socialMediaLinks.map((link) => (
+            <FooterLink key={link.href} {...link} />
+          ))}
+        </div>
+        <div className="mx-10 scroll-bottom-3">
+          <p className="font-semibold text-center text-gray-300 text-sm md:text-base">
+            © 2022 He4rt Devs. Todos os direitos reservados.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,14 @@
+import Footer from "~/components/footer";
+
 export default function Index() {
   return (
-    <div className="bg-purple-900 min-h-screen min-w-full flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-white">
-        He4rt Learning SSR with Remix...
-      </h1>
-    </div>
+    <>
+      <div className="bg-purple-900 w-full flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-white">
+          He4rt Learning SSR with Remix...
+        </h1>
+      </div>
+      <Footer />
+    </>
   );
 }
