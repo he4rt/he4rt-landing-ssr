6493b432a6c091f8cf2f5674119571066acb04ed
João Victor <joaovictorvisona@gmail.com>
2022-12-19 16:14:16 -0300

    chore: prettier setup

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,15 @@
-import Footer from "~/components/footer";
+import Footer from '~/components/Footer';
+import ForNoobs from '~/components/ForNoobs';
 
 export default function Index() {
   return (
     <>
-      <div className="bg-purple-900 w-full flex items-center justify-center">
-        <h1 className="text-3xl font-bold text-white">
+      <div className='bg-purple-900 w-full flex items-center justify-center'>
+        <h1 className='text-3xl font-bold text-white'>
           He4rt Learning SSR with Remix...
         </h1>
       </div>
+      <ForNoobs />
       <Footer />
     </>
   );
