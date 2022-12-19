7e94dc4d6c953063ef8196a9c4ba063d121c1bf4
m7he4rt <mateuspm2015@hotmail.com>
2022-12-19 19:51:59 -0300

    feat: create mock users

---
+++ app/mocks/users.ts
@@ -0,0 +1,70 @@
+import type { UserCardProps } from "~/components/userCard";
+
+export const users: UserCardProps[] = [
+  {
+    username: "Daniel Reis",
+    occupation: "Fundador",
+    avatar: "https://i.imgur.com/Ag5fBWb.png",
+    twitter: "https://twitter.com/danielhe4rt",
+  },
+  {
+    username: "Gustavo Lima",
+    avatar: "https://i.imgur.com/e2FFCWs.png",
+    occupation: "Co-fundador",
+    twitter: "https://twitter.com/kjkGustavo",
+  },
+  {
+    username: "Pride",
+    avatar: "https://i.imgur.com/1cCuTBM.png",
+    occupation: "Joker",
+    twitter: "https://twitter.com/He4rtDevs",
+  },
+  {
+    username: "Júlia Vaz",
+    avatar: "https://i.imgur.com/H17K8LR.png",
+    occupation: "Social Media",
+    twitter: "https://twitter.com/jujuvazzz",
+  },
+  {
+    username: "Felipe Gabriel",
+    avatar: "https://i.imgur.com/uc9feRB.png",
+    occupation: "Diretor de Operações ",
+    twitter: "https://twitter.com/setekpro",
+  },
+  {
+    username: "Giovane Silva",
+    avatar: "https://i.imgur.com/DivzI4u.png",
+    occupation: "Contribuidor Principal",
+    twitter: "https://twitter.com/He4rtDevs",
+  },
+  {
+    username: "Mateus Pinheiro",
+    avatar: "https://imgur.com/G3bazBS.png",
+    occupation: "Contribuidor Principal",
+    twitter: "https://twitter.com/m7ez1n",
+  },
+  {
+    username: "Luis Eduardo",
+    avatar: "https://i.imgur.com/Ef8lHZU.png",
+    occupation: "Líder de Moderação",
+    twitter: "https://twitter.com/Luis_Nadachi",
+  },
+  {
+    username: "Gabriel Vieira",
+    avatar: "https://i.imgur.com/e6LnPQ4.png",
+    occupation: "UI/UX Designer",
+    twitter: "https://twitter.com/nexturhe4rt",
+  },
+  {
+    username: "Gabriel Mazieri",
+    avatar: "https://i.imgur.com/GqiTkwI.png",
+    occupation: "Community Manager",
+    twitter: "https://twitter.com/grandehe4rt",
+  },
+  {
+    username: "Yasmin Felício",
+    avatar: "https://i.imgur.com/4yOwyk1.png",
+    occupation: "Community Manager",
+    twitter: "https://twitter.com/yayaflc",
+  },
+];
