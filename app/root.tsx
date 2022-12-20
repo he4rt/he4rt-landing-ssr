7dddfb806fc793da0811ddc5f0d06e5c0a4fbe89
slmeduardo <itsmeeduardoc@gmail.com>
2022-12-19 23:40:04 -0300

    feat: Find at He4rt Section

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction, MetaFunction } from '@remix-run/node';
+import type { LinksFunction, MetaFunction } from "@remix-run/node"
 import {
   Links,
   LiveReload,
@@ -6,51 +6,51 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from '@remix-run/react';
+} from "@remix-run/react"
 
-import styles from '~/tailwind.css';
+import styles from "~/tailwind.css"
 
-import favicon from '../public/favicon.svg';
+import favicon from "../public/favicon.svg"
 
 export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'He4rt Developers',
-  viewport: 'width=device-width,initial-scale=1',
-});
+  charset: "utf-8",
+  title: "He4rt Developers",
+  viewport: "width=device-width,initial-scale=1",
+})
 
 export const links: LinksFunction = () => [
-  { rel: 'stylesheet', href: styles },
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+  { rel: "stylesheet", href: styles },
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
   },
   {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700&family=Spline+Sans:wght@400;500;700&display=swap',
-    crossOrigin: 'anonymous',
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700&family=Spline+Sans:wght@400;500;700&display=swap",
+    crossOrigin: "anonymous",
   },
   {
-    rel: 'icon',
+    rel: "icon",
     href: favicon,
-    type: 'image/svg',
+    type: "image/svg",
   },
-];
+]
 
 export default function App() {
   return (
-    <html lang='pt-BR'>
+    <html lang="pt-BR">
       <head>
         <Meta />
         <Links />
       </head>
-      <body className='w-screen h-screen dark:bg-gray-999 font-spline'>
+      <body className="h-screen dark:bg-gray-999 font-spline">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
