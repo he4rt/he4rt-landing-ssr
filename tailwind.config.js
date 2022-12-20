27dfd6e09e255d38bab36630a6d3a2472c5510ae
m7he4rt <mateuspm2015@hotmail.com>
2022-12-19 23:49:20 -0300

    feat: create container

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,6 +55,15 @@ module.exports = {
         dm: ['DM Sans'],
       },
     },
+    container: {
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+        '2xl': '1496px'
+      }
+    }
   },
   plugins: [],
 };
