f385222db80e4d3e34fc791e357091f73ff6557d
m7he4rt <mateuspm2015@hotmail.com>
2022-12-21 16:28:05 -0300

    refactor: remove svgs add png background

--- app/components/He4rt.tsx
+++ app/components/He4rt.tsx
@@ -2,10 +2,10 @@ export default function He4rt() {
   return (
     <>
       <header
-        className='w-full lg:h-screen max-h-none md:max-h-[800px] max-w-7xl flex flex-col lg:flex-row items-center justify-between relative mx-auto'
+        className='w-full lg:h-screen max-h-none md:max-h-[800px] max-w-7xl flex flex-col lg:flex-row items-center justify-between mx-auto'
         id='inicio'
       >
-        <div className='flex flex-col max-w-5xl pr-6 relative z-10 lg:pb-11 mb-0 mt-1 lg:mt-0'>
+        <div className='flex flex-col max-w-5xl pr-6 lg:pb-11 mb-0 mt-1 lg:mt-0 pt-[100px] md:pt-0'>
           <h1 className='text-xl md:text-2xl lg:text-4xl text-[#424141] font-bold'>
             Feito pela{' '}
             <span className='text-purple-500 dark:text-purple-700'>
@@ -36,71 +36,11 @@ export default function He4rt() {
           </div>
         </div>
       </header>
-      <svg
-        width='60%'
-        viewBox='0 0 900 705'
-        fill='none'
-        xmlns='http://www.w3.org/2000/svg'
-        className='absolute top-0.5 right-0.5 md:right-2 md:top-2'
-      >
-        <path
-          d='M1 1C22.9185 119.5 159.708 345.2 531.517 300C631.119 292.833 844.459 355 901 661'
-          stroke='#782BF1'
-          stroke-width='.45rem'
-          stroke-linecap='round'
-          className='block md:hidden'
-        />
-        <path
-          d='M1 1C22.9185 119.5 159.708 345.2 531.517 300C631.119 292.833 844.459 355 901 661'
-          stroke='#782BF1'
-          stroke-width='2'
-          stroke-linecap='round'
-          className='hidden md:block'
-        />
-      </svg>
-      <svg
-        width='60%'
-        viewBox='0 0 896 658'
-        fill='none'
-        xmlns='http://www.w3.org/2000/svg'
-        className='absolute top-0 right-0'
-      >
-        <path
-          d='M530.517 297C158.708 342.2 21.9185 116.5 0 -2H900V658C843.459 352 630.119 289.833 530.517 297Z'
-          fill='url(#paint0_linear_7_93)'
-        />
-        <defs>
-          <linearGradient
-            id='paint0_linear_7_93'
-            x1='2.36829e-05'
-            y1='-2.5001'
-            x2='1014.438'
-            y2='235.537'
-            gradientUnits='userSpaceOnUse'
-          >
-            <stop stop-color='#782BF1' />
-            <stop offset='1' stop-color='#A62BF1' />
-            <stop offset='1' stop-color='#7935D8' />
-          </linearGradient>
-        </defs>
-      </svg>
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        width='66'
-        height='600'
-        viewBox='0 0 66 58'
-        fill='none'
-        className='absolute w-2/4 -right-[290px] -top-8 opacity-30'
-      >
-        <path
-          d='M48.9173 3.74037e-05C46.684 -0.00387328 44.472 0.434009 42.4086 1.28847C40.3452 2.14294 38.4713 3.39711 36.8946 4.97878L16.8528 25.0166L12.8452 21.009C12.3033 20.4863 11.871 19.8608 11.5736 19.1691C11.2761 18.4774 11.1194 17.7333 11.1126 16.9804C11.1059 16.2275 11.2492 15.4808 11.5342 14.7839C11.8192 14.0869 12.2403 13.4538 12.7727 12.9214C13.3051 12.389 13.9382 11.9679 14.6352 11.6829C15.3321 11.3979 16.0788 11.2546 16.8317 11.2613C17.5846 11.2681 18.3287 11.4248 19.0204 11.7223C19.7121 12.0197 20.3376 12.452 20.8604 12.9939L28.8794 4.97878C25.6773 1.85535 21.3734 0.119409 16.9003 0.147062C12.4272 0.174715 8.14505 1.96374 4.98175 5.12651C1.81846 8.28929 0.0287334 12.5711 0.000342964 17.0442C-0.0280475 21.5173 1.70718 25.8215 4.83008 29.0242L16.8528 41.0469L56.2467 1.65301C53.9583 0.559625 51.4534 -0.00529515 48.9173 3.74037e-05Z'
-          fill='#F3F3F3'
-        />
-        <path
-          d='M64.262 9.67212L48.9175 25.0206L44.9099 21.0091L36.8948 29.0242L40.9023 33.0318L24.8721 49.062L32.8872 57.0811L60.9402 29.0282C63.4266 26.5408 65.081 23.3441 65.6759 19.8778C66.2708 16.4116 65.7768 12.8461 64.262 9.67212Z'
-          fill='#F3F3F3'
-        />
-      </svg>
+      <img
+        src='/images/background-heart.png'
+        alt='Imagem com degradê roxo com a logo da He4rt Developers ao fundo desfocada'
+        className='absolute top-0 right-0 max-w-[65%] w-full'
+      />
     </>
   );
 }
