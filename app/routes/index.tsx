0174e1413e37890948951f8f1445b60eb0ab5b70
slmeduardo <itsmeeduardoc@gmail.com>
2022-12-28 19:35:57 -0300

    feat: partners section

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,6 +4,7 @@ import About from '~/components/About';
 import FindAtHe4rtSection from '~/components/FindAtHe4rtSection';
 import Footer from '~/components/Footer';
 import He4rt from '~/components/He4rt';
+import OurPartners from '~/components/OurPartners';
 import Staff from '~/components/Staff';
 
 import ForNoobs from '~/components/ForNoobs';
@@ -22,10 +23,19 @@ export default function Index() {
   return (
     <>
       <He4rt />
+<<<<<<< HEAD
+      <div className='container mx-auto py-40 px-10'>
+        <About />
+        <FindAtHe4rtSection />
+        <Staff />
+        <OurPartners />
+      </div>
+=======
       <About />
       <ForNoobs />
       <FindAtHe4rtSection />
       <Staff />
+>>>>>>> 60238feaafbd78cd89bc4a10ea6a5ae5c1fbcb4f
       <Footer />
     </>
   );
