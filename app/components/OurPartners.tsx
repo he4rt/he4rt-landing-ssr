35b5e4d5870b8eb6d134797575fa4f003eafff37
João Victor <joaovictorvisona@gmail.com>
2022-12-30 17:01:41 -0300

    fix: missin partnet picture and our partners section broken

--- app/components/OurPartners.tsx
+++ app/components/OurPartners.tsx
@@ -1,24 +1,23 @@
 import PartnerCard from './PartnerCard';
+import Container from '~/components/Container';
 
 export default function OurPartners() {
   return (
-    <>
-      <div className='pt-40'>
-        <div className='text-center mb-11'>
-          <h2 className='text-4xl text-gray-700 font-bold mb-2 font-spline'>
-            Nossos parceiros
-          </h2>
-          <p className='text-gray-750 font-sans'>
-            Nós temos os recursos para levar qualquer ideia para frente, nenhum
-            sonho é louco demais para nós. Descubra como desenvolver projetos
-            que revolucionam a maneira de aprender.
-          </p>
-        </div>
-        <div className='md:grid md:grid-cols-2 md:gap-10 lg:gap-20 flex flex-col gap-10'>
-          <PartnerCard />
-          <PartnerCard />
-        </div>
+    <Container>
+      <div className='text-center mb-11'>
+        <h2 className='text-4xl text-gray-700 font-bold mb-2 font-spline'>
+          Nossos parceiros
+        </h2>
+        <p className='text-gray-750 font-sans'>
+          Nós temos os recursos para levar qualquer ideia para frente, nenhum
+          sonho é louco demais para nós. Descubra como desenvolver projetos que
+          revolucionam a maneira de aprender.
+        </p>
       </div>
-    </>
+      <div className='md:grid md:grid-cols-2 md:gap-10 lg:gap-20 flex flex-col gap-10'>
+        <PartnerCard />
+        <PartnerCard />
+      </div>
+    </Container>
   );
 }
