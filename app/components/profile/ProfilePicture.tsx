5e2d95b9f4261de8fd2356761c0442c56a4450e4
João Victor <joaovictorvisona@gmail.com>
2022-12-31 18:03:41 -0300

    feat: profile season badge

--- app/components/profile/ProfilePicture.tsx
+++ app/components/profile/ProfilePicture.tsx
@@ -1,25 +1,30 @@
 import type { APPProfile } from '~/types/profile';
+import ProfileSeasonBadge from './ProfileSeasonBadge';
 
 type ProfilePictureProps = Pick<
   APPProfile,
-  'nickname' | 'level' | 'profilePicture'
+  'nickname' | 'level' | 'profilePicture' | 'seasonInfo'
 >;
 
 export default function ProfilePicture({
   profilePicture,
   nickname,
   level,
+  ...seasonInfo
 }: ProfilePictureProps) {
   return (
-    <div className='absolute -top-16 inline-flex flex-col items-center'>
-      <img
-        src={profilePicture}
-        alt={`Foto de ${nickname}`}
-        className='h-28 w-28 rounded-full border-8 border-purple-600'
-      />
-      <div className='absolute -bottom-2 inline-block rounded-lg bg-gray-50 px-3 py-1 font-bold text-purple-500'>
-        Lvl. {level}
+    <div className='absolute -top-16 inline-flex items-center justify-center '>
+      <div className='relative flex flex-col items-center'>
+        <img
+          src={profilePicture}
+          alt={`Foto de ${nickname}`}
+          className='h-28 w-28 rounded-full border-8 border-purple-600'
+        />
+        <div className='absolute -bottom-2 inline-block rounded-lg bg-gray-50 px-3 py-1 font-bold text-purple-500'>
+          Lvl. {level}
+        </div>
       </div>
+      <ProfileSeasonBadge {...seasonInfo} />
     </div>
   );
 }
---
+++ app/components/profile/ProfileSeasonBadge.tsx
@@ -0,0 +1,24 @@
+import type { APPProfile } from '~/types/profile';
+
+type ProfileSeasonBadgeProps = Pick<APPProfile, 'seasonInfo'>;
+
+export default function ProfileSeasonBadge({
+  seasonInfo,
+}: ProfileSeasonBadgeProps) {
+  if (!seasonInfo || !seasonInfo.rankingPosition || !seasonInfo.seasonName) {
+    return <></>;
+  }
+
+  return (
+    <span className='ml-8 flex items-center rounded-lg bg-white px-2 py-1 shadow-md'>
+      <img className='mr-3 h-4' src='/assets/icons/trophy.png' alt='' />
+      <span className='mr-1 font-bold text-gray-900'>
+        {seasonInfo.seasonName}
+      </span>
+      <span className='font-normal text-gray-700'>
+        {' | '}
+        {seasonInfo?.rankingPosition}º Lugar
+      </span>
+    </span>
+  );
+}
