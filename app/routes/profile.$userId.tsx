e1e6135e2f4aeb94bf1c84095da85012c83bdbc6
m7he4rt <mateuspm2015@hotmail.com>
2023-01-21 02:06:48 -0300

    feat: add badgeInfo in profile page

--- app/routes/profile.$userId.tsx
+++ app/routes/profile.$userId.tsx
@@ -1,11 +1,13 @@
 import type { LoaderArgs } from '@remix-run/node';
+import { badRequest } from 'remix-utils';
+import { useLoaderData } from '@remix-run/react';
+import { fetchProfile } from '~/services/fetchProfile';
+
+import ProfileBadge from '~/components/profile/ProfileBadge';
 import ProfileAboutMe from '~/components/profile/ProfileAboutMe';
 import ProfileBackground from '~/components/profile/ProfileBackground';
 import ProfileHeader from '~/components/profile/ProfileHeader';
 import ProfilePicture from '~/components/profile/ProfilePicture';
-import { badRequest } from 'remix-utils';
-import { useLoaderData } from '@remix-run/react';
-import { fetchProfile } from '~/services/fetchProfile';
 
 export const loader = async ({ params }: LoaderArgs) => {
   const userId = params['userId'];
@@ -23,13 +25,14 @@ export default function Profile() {
   const data = useLoaderData<LoaderType>();
 
   return (
-    <div className='h-screen w-screen bg-gray-50 font-dm'>
-      <div className='relative mx-auto h-full w-full max-w-screen-lg bg-white shadow-sm'>
+    <div className='w-screen h-screen bg-gray-50 font-dm'>
+      <div className='relative w-full h-full max-w-screen-lg mx-auto bg-white shadow-sm'>
         <ProfileBackground />
         <div className='relative px-8'>
           <ProfilePicture {...data} />
           <ProfileHeader {...data} />
           <ProfileAboutMe description={data.about} />
+          <ProfileBadge badgeInfo={data.badgeInfo} />
         </div>
       </div>
     </div>
