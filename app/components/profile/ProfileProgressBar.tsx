f084e31db6e3cb224cd9204d253cb00130a94554
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-02-04 00:36:34 -0300

    Change progress bar orientation
    Co-Authored-By: m7he4rt

--- app/components/profile/ProfileProgressBar.tsx
+++ app/components/profile/ProfileProgressBar.tsx
@@ -1,34 +1,34 @@
 type ProfileProgressBarProps = {
-  imageSrc: string,
-  imageAlt: string,
-  percentage: number,
-}
+  imageSrc: string;
+  imageAlt: string;
+  percentage: number;
+};
 
 export default function ProfileProgressBar({
   imageSrc,
   imageAlt,
-  percentage
+  percentage,
 }: ProfileProgressBarProps) {
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <div className="w-28 h-28 relative rounded-full">
+    <div className='flex h-full w-full items-center justify-center'>
+      <div className='relative h-28 w-28 rounded-full'>
         <img
-          className="w-full h-full rounded-full border-8 border-purple-300"
+          className='h-full w-full rounded-full border-[5px] border-purple-300'
           src={imageSrc}
           alt={imageAlt}
         />
 
         <svg
-          viewBox="0 0 100 100"
-          className="scale-[120%] absolute inset-0 w-full h-full text-center"
+          viewBox='0 0 100 100'
+          className='absolute inset-0 h-full w-full rotate-180 scale-[120%]'
         >
           <path
-            className="stroke-purple-500 stroke-[8px]"
-            id="progress"
-            strokeLinecap="round"
-            fill="none"
+            className='stroke-purple-500 stroke-[5px]'
+            id='progress'
+            strokeLinecap='round'
+            fill='none'
             strokeDasharray={`${(percentage / 100) * 251},251`}
-            d="M50 10 a 40 40 0 0 1 0 80 a 40 40 0 0 1 0 -80"
+            d='M50 10 a 40 40 0 0 1 0 80 a 40 40 0 0 1 0 -80'
           />
         </svg>
       </div>
